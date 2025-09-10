2861dffe1bdf9c28d7c9d66e8f5c3ecbc0b3e47d
Franco Zaina <119088313+francozaina@users.noreply.github.com>
2025-09-10 17:03:15 -0300

    Update Guia3.hs

--- Guia3.hs
+++ Guia3.hs
@@ -52,3 +52,71 @@ sumaDistintos x y z
     | x == y && x /=z = x+z
     | x == y && x == z && y /= z = z+y 
 
+esMulti:: Integer -> Integer -> Bool
+esMulti x y 
+    | mod x y == 0 = True
+    | otherwise = False
+
+digitoUnidades :: Integer -> Integer
+digitoUnidades n = abs n `mod` 10
+
+digitoDecenas :: Integer -> Integer
+digitoDecenas n = abs n `mod` 100
+
+estanRelacionados :: Integer -> Integer -> Bool
+estanRelacionados a b = a /= 0 && b /= 0 && (-a) `mod` b == 0 && (-a) `div` b /= 0
+
+productoInterno :: (Integer, Integer) -> (Integer, Integer) -> Integer
+productoInterno (a,b) (x,y) = a * x + b * y
+
+esParMenor :: (Integer, Integer) -> (Integer, Integer) -> (Bool, Bool)
+esParMenor (a,b) (x,y)
+    | a > x && b > y = (False, False) 
+    | a > x && b < y = (False, True)
+    | a < x && b > y = (True, False)
+    | a < x && b < y  = (True,True)
+
+distancia :: (Float, Float) -> Float
+distancia (a,b) = sqrt(a*a + b*b)
+
+sumaTerna :: (Integer,Integer,Integer) -> Integer
+sumaTerna (a,b,c) = a + b + c
+
+sumarSoloMultiplos :: (Integer,Integer,Integer) -> Integer -> Integer
+sumarSoloMultiplos (a,b,c) x
+    | esMulti a x == True && esMulti b x == True && esMulti c x == True = a+b+c
+    | esMulti a x == False && esMulti b x == False && esMulti c x == False = 0
+    | esMulti a x == True && esMulti b x == False && esMulti c x == False = a
+    | esMulti a x == False && esMulti b x == True && esMulti c x == False = b
+    | esMulti a x == False && esMulti b x == False && esMulti c x == True = c
+    | esMulti a x == True && esMulti b x == True && esMulti c x == False = a + b
+    | esMulti a x == True && esMulti b x == False && esMulti c x == True = a + c
+    | esMulti a x == False && esMulti b x == True && esMulti c x == True = b + c
+
+posPrimerPar:: (Integer,Integer,Integer) -> Integer
+posPrimerPar (a,b,c)
+    | mod a 2 == 0 = 0
+    | mod b 2 == 0 = 1
+    | mod c 2 == 0 = 2
+
+crearPar :: Integer -> Integer -> (Integer,Integer)
+crearPar a b = (a,b)
+
+invertir :: (Integer,Integer) -> (Integer, Integer)
+invertir (a,b) = (b,a)
+
+--Ejercicio 5
+funcionf :: Integer -> Integer
+funcionf x
+    | x <= 7 = x*x
+    | x > 7 = 2*x -1
+
+funciong :: Integer -> Integer
+funciong x 
+    | mod x 2 == 0 = div x 2
+    | otherwise = 3*x+1
+
+todosMenores :: (Integer,Integer,Integer) -> Bool
+todosMenores (a,b,c)
+    | funcionf a > funciong a && funcionf b > funciong b && funcionf c > funciong c = True
+    | otherwise = False
